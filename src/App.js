5ef0c60a7427d9cf4838c94c2535f21a3b3f5e4e
Yash Gupta <y.yashgupta03@gmail.com>
2023-02-04 13:28:46 +0530

    File Structure updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
 
 function App() {
   return (
     <>
       <Home />
+      <About />
       <Cart />
     </>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ProductDetail from "./ProductDetail";
 import Cart from "./Cart";
+import Navbar from "../components/Navbar";
 
 function Home() {
   return (
     <div>
+      <Navbar />
       <ProductDetail />
       <Cart />
     </div>
