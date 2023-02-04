fae60d7ccff282d7c9bd1a83f52c3fbf27f09aaf
Yash Gupta <y.yashgupta03@gmail.com>
2023-02-04 13:24:34 +0530

    file structure created

---
+++ src/pages/Cart.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Checkout from "./Checkout";
+function Cart() {
+  return (
+    <div>
+      <Checkout />
+    </div>
+  );
+}
+
+export default Cart;
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ProductDetail from "./ProductDetail";
+import Cart from "./Cart";
+
+function Home() {
+  return (
+    <div>
+      <ProductDetail />
+      <Cart />
+    </div>
+  );
+}
+
+export default Home;
